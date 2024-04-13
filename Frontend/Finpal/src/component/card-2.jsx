27f5fb92b752141d8a5b3a2ed3bf285ecd172202
Suja16 <sujal16@somaiya.edu>
2024-04-13 19:24:46 +0530

    dashboard

--- Frontend/Finpal/src/component/card-2.jsx
+++ Frontend/Finpal/src/component/card-2.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { Card, CardContent, Typography, Grid, Box, Button } from '@mui/material';
+import { blue } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
 
 function SaveCard() {
   return (
     <Card>
+      <Typography ml={2} variant="h5" component="div" style={{textAlign: "start"}} color={'blue'}>
+        Save More
+      </Typography>
       <CardContent>
         <Grid container spacing={0}>
           <Grid item xs={6}>
-            <Typography variant="h6" component="div">
-              Current Value
+            <Typography variant="body2" >
+             Spend on Needs
             </Typography>
-            <Typography variant="h6" color="text.secondary">
-              1000
+            <Typography variant="h6" >
+              60%
             </Typography>
           </Grid>
           <Grid item xs={4}>
@@ -39,6 +43,11 @@ function SaveCard() {
             </Typography>
           </Grid>
         </Grid>
+        <Box display={'flex'} justifyContent={'center'}>
+        <Button style={{color:'white',}} >
+          <Link to="/expanses">More Details</Link>  
+        </Button>        
+        </Box>
       </CardContent>
     </Card>
   );
--- Frontend/Finpal/src/pages/dashboard.jsx
+++ Frontend/Finpal/src/pages/dashboard.jsx
@@ -2,6 +2,8 @@ import { Box, Container, Typography } from "@mui/material";
 import ClickableChips from "../component/chips";
 import InfoCard from "../component/card";
 import Sidenav from "../component/sidenav";
+import SaveCard from "../component/card-2";
+import StockList from "../component/toptable";
 
 export const Dashboard = () => {
   return (
@@ -26,9 +28,15 @@ export const Dashboard = () => {
           link="https://www.google.com/finance/quote/NIFTY_50:INDEXNSE"
         />
       </Box>
-      <Box display={"flex"} justifyContent={"space-between"}>
+      <Box display={"flex"} justifyContent={"space-between"} mb={4}>
         <InfoCard />
-        <InfoCard />
+        <SaveCard />
+      </Box>
+      <Box mb={4} >
+      <StockList />
+      </Box>
+      <Box>
+      <iframe width="1150" height="400" src="https://rss.app/embed/v1/wall/trle22W9o8UTorHS" frameborder="0"></iframe>
       </Box>
     </Container>
     </Box>
