7cd7478a9854e483a2e8c8000d0a4ca5c09f06f2
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2024-04-14 01:06:50 +0530

    risk assessment

--- Backend/routes/risk_assessment.py
+++ Backend/routes/risk_assessment.py
@@ -1,6 +1,24 @@
-import random
+from flask import Flask, request, redirect, Blueprint, render_template, jsonify
+import requests
+from datetime import datetime
+from credentials.firebase_config import db
+import numpy as np
+from sklearn.linear_model import LinearRegression
 import statistics
+import random
 
+# risk_levels = {
+#     'stocks': 5,
+#     'bonds': 4,
+#     'cash': 1,
+#     'gold': 3,
+#     'real_estate': 2,
+#     'others': 2
+# }
+
+risk_assessment_bp = Blueprint('risk_assessment', __name__)
+
+@risk_assessment_bp.route('/risk_assessment', methods=['GET', 'POST'])
 def monte_carlo_risk_assessment(total_overall_investment, total_overall_returns, stocks, bonds, cash, gold, real_estate, others, num_simulations):
     risk_factors = {
         'stocks': 0.9,  
@@ -62,16 +80,47 @@ def monte_carlo_risk_assessment(total_overall_investment, total_overall_returns,
         'Risk Assessment': risk_assessment
     }
 
-result = monte_carlo_risk_assessment(
-    total_overall_investment=100000,
-    total_overall_returns=80000,    
-    stocks=0.4,
-    bonds=0.3,
-    cash=0.1,
-    gold=0.1,
-    real_estate=0.1,
-    others=0.0,
-    num_simulations=100
-)
 
-print(result)
+
+
+# def prepare_data(total_overall_investment, stocks, bonds, cash, gold, real_estate, others):
+#     total_investment = stocks + bonds + cash + gold + real_estate + others
+#     proportions = {
+#         'stocks': stocks / total_investment,
+#         'bonds': bonds / total_investment,
+#         'cash': cash / total_investment,
+#         'gold': gold / total_investment,
+#         'real_estate': real_estate / total_investment,
+#         'others': others / total_investment
+#     }
+    
+#     features = np.array(list(proportions.values())).reshape(1, -1)
+#     target = np.array([sum(risk_levels[asset] * proportion for asset, proportion in proportions.items())])
+#     return features, target
+
+
+# def train_model():
+    
+#     X_train = np.array([[0.8, 0.1, 0.05, 0.05, 0.0, 0.0]]) 
+#     y_train = np.array([4.5]) 
+#     model = LinearRegression().fit(X_train, y_train)
+#     return model
+
+
+# def risk_assessment(total_overall_investment, stocks, bonds, cash, gold, real_estate, others):
+#     features, target = prepare_data(total_overall_investment, stocks, bonds, cash, gold, real_estate, others)
+#     model = train_model() 
+#     predicted_risk = model.predict(features)
+#     return predicted_risk[0]
+
+# # Example usage
+# total_overall_investment = 1000000
+# stocks = 800000
+# bonds = 100000
+# cash = 50000
+# gold = 50000
+# real_estate = 50000
+# others = 0
+
+# risk_level = risk_assessment(total_overall_investment, stocks, bonds, cash, gold, real_estate, others)
+# print(f"Predicted Risk Level: {risk_level}")
